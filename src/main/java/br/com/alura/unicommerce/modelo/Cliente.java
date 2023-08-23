4a040ec7ae88178495bae6e76802122318ca195e
mauricio <mauricio@uniformg.edu.br>
2023-08-23 17:00:46 -0300

    subindo todas as alterações do projeto principal, parei na criação das entidades.

---
+++ src/main/java/br/com/alura/unicommerce/modelo/Cliente.java
@@ -0,0 +1,46 @@
+package br.com.alura.unicommerce.modelo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "cliente")
+public class Cliente {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	
+	@Column(name = "nome", length = 150)
+	private String nome;
+	
+	public Cliente(String nome) {
+		this.nome = nome;
+	}
+	
+	public Cliente () {
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}	
+	
+}
