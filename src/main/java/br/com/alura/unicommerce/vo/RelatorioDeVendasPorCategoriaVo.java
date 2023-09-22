c8c020b8e91215915e4e5381a596b0fa56abe7ad
mauricio <mauricio@uniformg.edu.br>
2023-09-22 20:47:39 -0300

    Ajustando controller

---
+++ src/main/java/br/com/alura/unicommerce/vo/RelatorioDeVendasPorCategoriaVo.java
@@ -0,0 +1,37 @@
+package br.com.alura.unicommerce.vo;
+
+import java.math.BigDecimal;
+
+public class RelatorioDeVendasPorCategoriaVo {
+	private String nomeCategoria;
+	private long quantidadeVendida;
+	private BigDecimal montante;
+
+	public RelatorioDeVendasPorCategoriaVo() {
+	}
+
+	public RelatorioDeVendasPorCategoriaVo(String nomeProduto, long quantidadeVendida, BigDecimal montante) {
+		super();
+		this.nomeCategoria = nomeProduto;
+		this.quantidadeVendida = quantidadeVendida;
+		this.montante = montante;
+	}
+
+	public String getNomeProduto() {
+		return nomeCategoria;
+	}
+
+	public long getQuantidadeVendida() {
+		return quantidadeVendida;
+	}
+
+	public BigDecimal getMontante() {
+		return montante;
+	}
+
+	@Override
+	public String toString() {
+		return "Relatorio De Vendas [Nome da Categoria = " + nomeCategoria + ", quantidade Vendida = " + quantidadeVendida
+				+ ", montante = " + montante + "]";
+	}	
+}
