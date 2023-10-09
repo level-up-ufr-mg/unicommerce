ac9acbcc463a3d762a226fd03ebecb1b976dcd70
Mauricio <mauricio@uniformg.edu.br>
2023-10-09 07:31:54 -0300

    Continuando Semana-05

---
+++ src/main/java/br/com/alura/unicommerce/SpringBootStarter.java
@@ -0,0 +1,11 @@
+package br.com.alura.unicommerce;
+
+import org.springframework.boot.SpringApplication;
+
+public class SpringBootStarter {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApplication.class, args);
+	}
+
+}
